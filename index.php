d4a912f48317492d65a0b3fb185a69d24ac169bf
mvakili <m.vakili95@gmail.com>
2016-03-17 15:12:18 +0330

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php 
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
+//error_reporting(E_ALL);
+//ini_set('display_errors', 1);
 require_once 'rb_flight/setup.php';
 
 init();
@@ -12,6 +12,4 @@ if ( isset($opts['i']))
 {
 	include 'src/run.php';
 }
-
-
 ?>
